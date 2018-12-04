ae52222026a770e263a2dcfc02c1f0d68146534c
shreya <sk2645@njit.edu>
2018-12-04 18:44:53 -0500

    TASK:Update

--- config/database.php
+++ config/database.php
@@ -42,16 +42,7 @@ return [
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
-        'pg-heroku' => [
-            'driver' => 'pgsql',
-            'host' => $heroku_db_url['host'],
-            'database' => substr($heroku_db_url['path'], 1),
-            'username' => $heroku_db_url['user'],
-            'password' => $heroku_db_url['pass'],
-            'charset' => 'utf8',
-            'prefix' => '',
-            'schema' => 'public',
-        ],
+
 
 
         'mysql' => [
@@ -94,6 +85,17 @@ return [
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
+        ],
+
+        'pg-heroku' => [
+            'driver' => 'pgsql',
+            'host' => $heroku_db_url['host'],
+            'database' => substr($heroku_db_url['path'], 1),
+            'username' => $heroku_db_url['user'],
+            'password' => $heroku_db_url['pass'],
+            'charset' => 'utf8',
+            'prefix' => '',
+            'schema' => 'public',
         ],
 
     ],
