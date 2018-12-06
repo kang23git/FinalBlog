d09cea44ed6ccaf6d1467f6c2d029aaac4ba1d5a
shreya <sk2645@njit.edu>
2018-12-06 10:44:33 -0500

    TASK:Created views for profile, questions and answers.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,8 +6,9 @@
             <div class="col-md-12">
                 <div class="card">
                     <div class="card-header">Questions
-                        <a class="btn btn-primary float-right" href="#">
-                            Create a Question
+
+                        <a class="btn btn-primary float-right" href="{{ route('questions.create') }}">
+                            Create a question.
                         </a>
 
                         <div class="card-body">
@@ -29,7 +30,7 @@
                                             <div class="card-footer">
                                                 <p class="card-text">
 
-                                                    <a class="btn btn-primary float-right" href="{{ route('question.show', ['id' => $question->id]) }}">
+                                                    <a class="btn btn-primary float-right" href="{{ route('questions.show', ['id' => $question->id]) }}">
                                                         View
                                                     </a>
                                                 </p>
