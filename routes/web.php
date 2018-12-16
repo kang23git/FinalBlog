066a784a66dee7f144e7e74471437b2434d0b58c
shreya <sk2645@njit.edu>
2018-12-16 12:39:59 -0500

    TASK:Created post form view.

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,4 @@ Route::delete('/questions/{question_id}/answer/{answer_id}', 'AnswerController@d
 Route::resources([
     'questions' => 'QuestionController',
 ]);
+Route::resource('posts','PostController');
