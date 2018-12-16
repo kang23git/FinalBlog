40c81332f5fc788507bb21d341d762d8ddd8f4fe
shreya <sk2645@njit.edu>
2018-12-16 15:27:21 -0500

    TASK:Added date and timestamp for posts from database.

--- resources/views/postShow.blade.php
+++ resources/views/postShow.blade.php
@@ -7,18 +7,46 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap-->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-@section('title', '| View Post')
+    @section('title', '| View Post')
 
-@section('content')
+    @section('content')
 
-    <h1>{{$post -> title }}</h1>
+        <div class="=row">
+            <div class="col-md-8">
+                <h1>{{$post -> title }}</h1>
 
-    <p class="lead">{{$post -> body}}</p>
+                <p class="lead">{{$post -> body}}</p>
+            </div>
+            <div class="col-md-4">
+                <div class="well">
+                    <dl class="dl-horizontal">
+                        <dt>Created At:</dt>
+                        <dd>{{ date('M j, Y h:ia', strtotime($post -> created_at)) }}</dd>
+                    </dl>
+                    <dl class="dl-horizontal">
+                        <dt>Updated At:</dt>
+                        <dd>{{ date('M j, Y h:ia', strtotime($post -> updated_at)) }}</dd>
+                    </dl>
+                    <hr>
+                    <div class="row">
+                        <div class="col-sm-6">
+                            {!! Html::linkRoute('posts.edit', 'Edit', array($post->id), array('class' => 'btn btn-primary btn-block' )) !!}
 
+                        </div>
+                        <div class="col-sm-6">
+                            {!! Html::linkRoute('posts.destroy', 'Delete', array($post->id), array('class' => 'btn btn-danger btn-block' )) !!}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 @endsection
 
 <!-- JavaScript-->
 
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+            integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+            crossorigin="anonymous"></script>
